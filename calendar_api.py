75859323662d1012d02124857173ad59b094009a
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-08-28 11:20:45 +0900

    add: googleログインを実装

--- calendar_api.py
+++ calendar_api.py
@@ -1,2 +1,29 @@
-if __name__ == '__main__':
-    print("This is calendar_api.py")
+import os
+from bottle import get, request, redirect
+from requests_oauthlib import OAuth2Session
+
+REDIRECT_URI = "https://hacku-techlion.herokuapp.com/google/oauth-callback"
+TOP_URL = "https://hacku-techlion.herokuapp.com/dummy"
+AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
+TOKEN_URI = "https://oauth2.googleapis.com/token"
+SCOPE = "https://www.googleapis.com/auth/calendar.readonly"
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+
+@get("/google/login")
+def google_login():
+    callback_query = "?callback=" + request.params.get("callback", TOP_URL)
+    oauth = OAuth2Session(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI + callback_query, scope=SCOPE)
+    authorization_url, state = oauth.authorization_url(AUTH_URI, access_type="offline", prompt="select_account")
+    print(state)
+    return redirect(authorization_url)
+
+
+@get("/google/oauth-callback")
+def google_oauth_callback():
+    oauth = OAuth2Session(client_id=CLIENT_ID, redirect_uri=REDIRECT_URI)
+    token = oauth.fetch_token(token_url=TOKEN_URI, authorization_response=request.url, client_secret=CLIENT_SECRET)
+    return dict(token)
+

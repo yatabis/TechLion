9782ca53bb07cc17925b38e0125753a8f9b1aaba
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-08-29 20:44:18 +0900

    update: sign-up関数のリファクタリング

---
+++ http_elements.py
@@ -0,0 +1,34 @@
+import json
+from bottle import HTTPResponse, BaseRequest
+from typing import Dict, Tuple, Optional
+
+JSON_HEADER = {"Content-Type": "application/json"}
+
+
+class Error:
+    def __init__(self, code: int, message: str):
+        self.code = code
+        self.body = {"error": {
+            "code": code,
+            "message": message
+        }}
+        self.response = HTTPResponse(status=code,
+                                     body=json.dumps(self.body, ensure_ascii=False),
+                                     headers={"Content-Type": "application/json"})
+
+
+def post_validation(req: BaseRequest, *args) -> Tuple[dict, Optional[Error]]:
+    if req.method != "POST":
+        return {}, Error(405, "GET method is not allowed.")
+    if req.get_header("Content-Type") != "application/json":
+        return {}, Error(400, "HTTP request header `Content-Type` is invalid.")
+    if req.body.getvalue().decode() == "":
+        return {}, Error(400, "Request body is empty.")
+    for k in args:
+        if not req.json.get(k):
+            return {}, Error(400, f"The field {k} does not exist in the request body.")
+    return req.json, None
+
+
+def json_response(status_code: int, body: dict) -> HTTPResponse:
+    return HTTPResponse(status=status_code, body=json.dumps(body, ensure_ascii=False), headers=JSON_HEADER)
